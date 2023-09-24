23b6ec3eb6b3ab172f6383c953a56ea8736fb1a5
dendiriki <dendirikirahmawan@gmail.com>
2023-09-24 15:49:47 +0700

    belajar 24-09-2023

--- 12-contact/app.js
+++ 12-contact/app.js
@@ -1,11 +1,57 @@
-const contacts = require ('./contacts');
+const yargs = require("yargs");
+const contacts = require('./contacts')
 
-const main = async () =>{
-    const nama = await contacts.tulisPertanyaan('Masukan nama anda :');
-    const email = await contacts.tulisPertanyaan('Masukan email anda :');
-    const noHP = await contacts.tulisPertanyaan('Masukan no-hp anda :');
 
-    contacts.simpanConstant(nama,email,noHP);
-}
+yargs.command({
+    command: 'add',
+    describe: 'Menambahkan contact baru',
+    builder: {
+     nama:{  
+        describe: 'Nama Lengakap',
+        demandOption: true,
+        type : 'string',
+        },
+    email:{
+        describe: 'Email',
+        demandOption: false,
+        type : 'string',
+    },
+    noHP: {
+        describe: 'Nomer Handphones',
+        demandOption: true,
+        type : 'string',
+    }, 
+   },
+   handler: function(argv){
+    contacts.simpanConstant(argv.nama, argv.email, argv.noHP)
+   }
+}).demandCommand();
 
-main();
+//menampilkan daftar semua nama dan no hp contact
+yargs.command({
+    command: 'list',
+    describe: 'menampilkan informasi semua kontak',
+   handler(){
+    contacts.listContact();
+   },
+});
+
+
+//menampilkan detail contact
+yargs.command({
+    command: 'detail',
+    describe: 'menampilkan detail contact berdasarkan nama ',
+    builder:{
+        nama: {
+            describe : 'nama lengkap',
+            demandOption : true,
+            type:'string'
+        },
+    },
+   handler(argv){
+    contacts.detailContact(argv.nama);
+   },
+});
+
+
+yargs.parse();
--- 12-contact/contacts.js
+++ 12-contact/contacts.js
@@ -1,11 +1,6 @@
 const fs = require('fs');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
+const chalk = require('chalk');
+const validator = require('validator');
 
 //membuat folder jika belum ada folder
 const dirPath = './data';
@@ -19,26 +14,82 @@ if (!fs.existsSync(dataPath)){
     fs.writeFileSync(dataPath, '[]', 'utf8');
 }
 
-const tulisPertanyaan = (pertanyaan) =>{
-    return new Promise((resolve, reject) =>{
-        rl.question(pertanyaan,(nama)=>{
-            resolve(nama);
-        });
-    });
-};
+const loadContact = () =>{
+    const fileBuffer = fs.readFileSync('data/contacts.json','utf-8');
+    const contacts = JSON.parse(fileBuffer);
+
+    return contacts;
+}
 
 const simpanConstant = (nama, email, noHP)=>{
     const contact = {nama, email, noHP};
-    const fileBuffer = fs.readFileSync('data/contacts.json','utf-8');
-    const contacts = JSON.parse(fileBuffer);
+    // const fileBuffer = fs.readFileSync('data/contacts.json','utf-8');
+    // const contacts = JSON.parse(fileBuffer);
+    const contacts = loadContact();
+
+
+    //cek apakah data duplikat
+    const duplikat = contacts.find((contact) => contact.nama === nama);
+    if(duplikat){
+        console.log(
+            chalk.red.inverse.bold('nama yang anda gunakan adalah duplikat silahkan gunakan nama yang lain')
+            );
+        return false;
+    }
+
+    //cek email contact
+if (email){
+    if(!validator.isEmail(email)){
+        console.log(
+            chalk.red.inverse.bold('email yang anda gunakan bukanlah email yang valid')
+            );
+        return false;
+    }
+}
+
+//cek no hanphone
+if(!validator.isMobilePhone(noHP,'id-ID')){
+    console.log(
+        chalk.red.inverse.bold('Nomber HP tidak valid')
+        );
+    return false;
+}
 
     contacts.push(contact);
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
 
-    console.log('terima kasih sudah memasukan data.');
-    rl.close();
+    console.log(chalk.green.inverse.bold('terima kasih sudah memasukan data.'));
 };
 
-module.exports = {tulisPertanyaan, simpanConstant};
+const listContact = () => {
+    const contacts = loadContact();
+    console.log(chalk.cyan.inverse.bold('Berikut adalah daftar kontak.'));
+    contacts.forEach((contact, i) => {
+        console.log(`${i + 1}. ${contact.nama} - ${contact.email} - ${contact.noHP}`);
+    });
+};
+
+//menampilkan detail dari contact
+const detailContact = (nama) =>{
+    const contacts = loadContact();
+
+    const contact = contacts.find(
+        (contact) => contact.nama.toLowerCase() === nama.toLowerCase()
+    );
+
+    if(!contact) {
+        console.log(chalk.red.inverse.bold(`tidak ada nama ${nama} kontak seperti yang anda maksut`));
+        return false;
+    };
+
+    console.log(chalk.cyan.inverse.bold(contact.nama));
+    console.log(contact.noHP);
+    if(contact.email){
+        console.log(contact.email);
+    };
+
+};
+
+module.exports = {simpanConstant, listContact, detailContact};
 
